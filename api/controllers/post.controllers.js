464ef4cb0349d0c50ac7fb7c67763e2fcdb92061
DuaaAlk <engduaa2013@gmail.com>
2022-03-13 09:53:02 +0300

    add post controllers

--- api/controllers/post.controllers.js
+++ api/controllers/post.controllers.js
@@ -1,1 +1,25 @@
-exports.fetchPosts = async (req, res, next) => {};
+const Post = require("../../database/models/Post");
+
+exports.fetchPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.fetchPost = async (postId, next) => {
+  try {
+    const post = await Post.findById(postId);
+    console.log("This is controller:", post);
+    if (post) return post;
+    else {
+      const err = new Error("Post not found");
+      err.status = 404;
+      next(err);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
